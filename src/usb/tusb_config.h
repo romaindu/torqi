ba49cbe7566e9c756c28fd7d2e6c92118c16f3ef
Romain Durand <romain58400@gmail.com>
2020-01-04 11:07:56 +0100

    space to tabs

--- src/usb/tusb_config.h
+++ src/usb/tusb_config.h
@@ -1,12 +1,12 @@
 #ifndef TUSB_CONFIG_H
 #define TUSB_CONFIG_H
 
-#define CFG_TUSB_DEBUG			0
+#define CFG_TUSB_DEBUG          0
 
-#define CFG_TUSB_MCU 			OPT_MCU_SAMD21
+#define CFG_TUSB_MCU            OPT_MCU_SAMD21
 
-#define CFG_TUSB_RHPORT0_MODE 	OPT_MODE_DEVICE
-#define CFG_TUSB_OS 			OPT_OS_NONE
+#define CFG_TUSB_RHPORT0_MODE   OPT_MODE_DEVICE
+#define CFG_TUSB_OS             OPT_OS_NONE
 
 #define CFG_TUD_CDC             1
 #define CFG_TUD_CDC_EPSIZE      64
@@ -14,6 +14,6 @@
 #define CFG_TUD_CDC_RX_BUFSIZE  CFG_TUD_CDC_EPSIZE
 
 #define CFG_TUD_HID             1
-#define CFG_TUD_HID_BUFSIZE 	16
+#define CFG_TUD_HID_BUFSIZE     16
 
 #endif
